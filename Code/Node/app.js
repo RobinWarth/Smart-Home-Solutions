d091cbc8ecef344e13b590ed8d763f06a2783f85
RobinWarth <robin.warth@gmail.com>
2017-11-14 19:31:14 +0100

    bug fixes

--- Code/Node/app.js
+++ Code/Node/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var app = express();
 var bodyParser = require("body-parser");
+var app = express();
+
 
 app.use(express.static('public'));
 
@@ -8,24 +9,32 @@ app.use("/styles",  express.static(__dirname + '/public/css'));
 app.use("/scripts", express.static(__dirname + '/public/js'));
 app.use("/images",  express.static(__dirname + '/public/images'));
 
-
+/*
 //testing python execution
 var spawn = require("child_process").spawn;
 var process = spawn('python',["../elropi.py", 1, 1]);
+
 
 process.stdout.on('data', function (data){
 	console.log(data.toString('utf8')); // buffer to string);
 });
 
+*/
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
-app.post('/input', function (req, res) {
+app.post('/input', urlencodedParser, function (req, res) {
+console.log(req.body);
   //testing python execution
+  var id = req.body.id;
+  var status = req.body.status;
+  
   var spawn = require("child_process").spawn;
-  var process = spawn('python',["../elropi.py", 1, 1]);
+  var process = spawn('python',["../elropi.py", id, status]);
 });
 
 app.listen(3000, function () {
