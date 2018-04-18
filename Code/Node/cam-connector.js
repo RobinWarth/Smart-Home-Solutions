09aa93f656b8be665f7fbc4a3057a92717efe448
RobinWarth <robin.warth@gmail.com>
2018-04-18 14:05:05 +0200

    minor changes

--- Code/Node/cam-connector.js
+++ Code/Node/cam-connector.js
@@ -11,51 +11,55 @@ let camera = new RaspiCam({
 
 exports.switchOnOff = (value) => {
 
-    if(value === "ON"){
+    if (value === "ON") {
         startRecording();
-    }else if (value === "OFF") {
+    }
+    else if (value === "OFF") {
         stopRecording();
-    }else{
+    }
+    else {
         console.log("unknown value!");
     }
-    
+
 };
 
 let startRecording = () => {
     console.log("camera: start recording!");
-    
+
     // Dateinamen aus Datum und Uhrzeit erzeugen
     let d = new Date();
     let time_string = d.toTimeString();
-    let time = time_string.split(' ')[0].replace(/:/g , "-");
-    
+    let time = time_string.split(' ')[0].replace(/:/g, "-");
+
     let date = d.toString();
     let dates = date.split(' ');
     let date_string = dates[2].concat(dates[1].concat(dates[3]));
-    
-    let full_date = date_string.concat("_".concat(time)); 
-    
+
+    let full_date = date_string.concat("_".concat(time));
+
     // Output-Directory: Anlegen, falls nicht schon vorhanden
     let new_dir = "/../../../Records/";
     let path = process.cwd().concat(new_dir);
-    
+
     console.log(path);
-    if(!fs.existsSync(path)){
+    if (!fs.existsSync(path)) {
         console.log("to create");
         fs.mkdirSync(path);
     }
-    
+
     let output = path.concat(full_date, ".h264");
-    
+
     camera.set("output", output);
-    
-    camera.start();
+    killFFServerProcess(() => {
+        camera.start();
+    });
+    //camera.start();
 };
 
 let stopRecording = () => {
     console.log("camera: stop recording!");
     console.log("my output is: " + camera.get("output"));
-    
+
     camera.stop();
 };
 
@@ -64,42 +68,71 @@ let stopRecording = () => {
 let convertLastVideoToMp4 = () => {
     let outputH264 = camera.get("output");
     let outputMp4 = outputH264.slice(0, outputH264.indexOf("h264")) + "mp4";
-    
-    const ls = spawn('ffmpeg', ['-framerate', '50', '-i', outputH264, '-c', 'copy', outputMp4]);
-    
+
+    let ls = spawn('ffmpeg', ['-framerate', '50', '-i', outputH264, '-c', 'copy', outputMp4]);
+
 
     ls.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
-    
-    ls.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    });
-    
-    ls.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
+        console.log(`stdout: ${data}`);
     });
 
-    
+    ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+
     //exec("ffmpeg -framerate 24 -i " + outputH264 + " -c copy " + outputMp4);
 };
 
+let killFFServerProcess = (callback) => {
+
+    let killignProcess = spawn('pkill', ['-f', 'ffserver']);
+
+
+    killignProcess.stdout.on('data', (data) => {
+        console.log(`killignProcess - stdout: ${data}`);
+    });
+
+    killignProcess.stderr.on('data', (data) => {
+        console.log(`killignProcess - stderr: ${data}`);
+    });
+
+    killignProcess.on('close', (code) => {
+        console.log(`killignProcess - child process exited with code ${code}`);
+        callback();
+    });
+
+};
+
+let startFFserverProcess = () => {
+    let startingProcess = spawn('../../Install-Scripts/reconfigure_ffmpeg');
+
+
+    startingProcess.on('close', (code) => {
+        console.log(`startingProcess - child process exited with code ${code}`);
+    });
+};
 
 
 // callbacks
-camera.on("start", function( err, timestamp ){
-	console.log("video started at " + timestamp );
+camera.on("start", function(err, timestamp) {
+    console.log("video started at " + timestamp);
 });
 
-camera.on("read", function( err, timestamp, filename ){
-	console.log("video captured with filename: " + filename + " at " + timestamp );
+camera.on("read", function(err, timestamp, filename) {
+    console.log("video captured with filename: " + filename + " at " + timestamp);
 });
 
-camera.on("exit", function( timestamp ){
-	console.log("video child process has exited at " + timestamp );
-	convertLastVideoToMp4();
+camera.on("exit", function(timestamp) {
+    console.log("video child process has exited at " + timestamp);
+    convertLastVideoToMp4();
 });
 
-camera.on("stop", function( err, timestamp ){
-	console.log("video child process was stopped at " + timestamp );
+camera.on("stop", function(err, timestamp) {
+    console.log("video child process was stopped at " + timestamp);
+    startFFserverProcess();
 });
