46c484ebbae61007a5c1328ad4a19758054224f2
RobinWarth <robin.warth@gmail.com>
2018-04-19 10:22:10 +0200

    improved performance and fixed bugs

--- Code/Node/cam-connector.js
+++ Code/Node/cam-connector.js
@@ -1,6 +1,182 @@
-const RaspiCam = require('raspicam');
 const fs = require('fs');
 const { spawn } = require('child_process');
+
+class CamController {
+
+    constructor() {
+
+        this.initalCheckForRunningFFServerProcess = true;
+
+        this.time = 3600000;
+
+        let new_dir = "/../../../Records/";
+        this.path = process.cwd().concat(new_dir);
+
+
+        if (!fs.existsSync(this.path)) {
+            console.log("to create");
+            fs.mkdirSync(this.path);
+        }
+        this.output = this.path.concat("default", ".h264"); //default
+
+        this.raspividProcess = null;
+
+        this.killingFFServerProcess = null;
+
+        this.ffserverProcess = null;
+        this.initialFFServerStart();
+
+    }
+
+
+    startRecording() {
+
+        let d = new Date();
+        let time_string = d.toTimeString();
+        let time = time_string.split(' ')[0].replace(/:/g, "-");
+
+        let date = d.toString();
+        let dates = date.split(' ');
+        let date_string = dates[2].concat(dates[1].concat(dates[3]));
+
+        let full_date = date_string.concat("_".concat(time));
+
+        this.output = this.path.concat(full_date, ".h264");
+
+
+
+
+        // called every time, when FFServer Process is killed to start raspivid (-> only reason to kill ffserver)
+        this.setFFServerExitListener(() => {
+
+            // raspivid -o video.h264 -t 10000
+            this.raspividProcess = spawn('raspivid', ['-o', this.output, '-t', this.time]);
+
+            this.setAllRaspividListeners();
+        });
+        this.killFFServerProcess();
+
+
+    }
+
+    stopRecording() {
+        console.log("camera: stop recording!");
+        //console.log("my output is: " + this.camera.get("output"));
+
+        if (this.raspividProcess) {
+            this.raspividProcess.kill();
+            //this.raspividProcess = null;
+        }
+
+    }
+
+    setAllRaspividListeners() {
+        this.setRaspividOutDataListener();
+        this.setRaspividOutErrListener();
+        this.setRaspividExitListener();
+    }
+
+    setRaspividOutDataListener() {
+        this.raspividProcess.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+    }
+
+    setRaspividOutErrListener() {
+        this.raspividProcess.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+    }
+
+    setRaspividExitListener() {
+        this.raspividProcess.on('exit', (code) => {
+            console.log(`child process exited with code ${code}`);
+            this.convertLastVideoToMp4();
+            this.startFFserverProcess();
+        });
+    }
+
+    /* need?
+    setFFServerCloseListener() {
+        this.ffserverProcess.on('close', (code) => {
+            console.log(`ffserverProcess - child process exited with code ${code}`);
+        });
+    }
+    */
+    setFFServerErrListener(callback) {
+        this.ffserverProcess.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+    }
+
+    setFFServerOutListener(callback) {
+        this.ffserverProcess.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+    }
+
+    setFFServerExitListener(callback) {
+        this.ffserverProcess.on('exit', (code) => {
+            console.log(`ffserverProcess - child process exited with code ${code}`);
+            callback();
+        });
+    }
+
+    startFFserverProcess() {
+        this.ffserverProcess = spawn('../../Install-Scripts/reconfigure_ffmpeg');
+        this.setFFServerExitListener(() => {});
+    }
+
+    initialFFServerStart() {
+        this.killFFServerProcess();
+
+        this.setKillingFFServerProcessExitListener(() => {
+            if (this.initalCheckForRunningFFServerProcess) {
+                this.initalCheckForRunningFFServerProcess = false;
+                this.startFFserverProcess();
+            }
+        });
+    }
+
+
+    setKillingFFServerProcessExitListener(callback) {
+        this.killingFFServerProcess.on('exit', (code) => {
+            console.log(`killignProcess - child process exited with code ${code}`);
+            callback();
+        });
+    }
+
+    killFFServerProcess() {
+        this.killingFFServerProcess = spawn('pkill', ['-f', 'ffserver']);
+    }
+
+
+    convertLastVideoToMp4() {
+        let outputH264 = this.output;
+        let outputMp4 = outputH264.slice(0, outputH264.indexOf("h264")) + "mp4";
+
+        let ls = spawn('ffmpeg', ['-framerate', '50', '-i', outputH264, '-c', 'copy', outputMp4]);
+        console.log("convert to mp4...");
+
+
+        ls.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        ls.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+        });
+
+        ls.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+
+    }
+
+
+}
+
+
 
 let myCam = new CamController();
 
@@ -17,166 +193,3 @@ exports.switchOnOff = (value) => {
     }
 
 };
-
-let killFFServerProcess = (callback) => {
-
-    let killignProcess = spawn('pkill', ['-f', 'ffserver']);
-
-
-    killignProcess.stdout.on('data', (data) => {
-        console.log(`killignProcess - stdout: ${data}`);
-    });
-
-    killignProcess.stderr.on('data', (data) => {
-        console.log(`killignProcess - stderr: ${data}`);
-    });
-
-    killignProcess.on('close', (code) => {
-        console.log(`killignProcess - child process exited with code ${code}`);
-        callback();
-    });
-
-};
-
-let startFFserverProcess = () => {
-    let startingProcess = spawn('../../Install-Scripts/reconfigure_ffmpeg');
-
-
-    startingProcess.on('close', (code) => {
-        console.log(`startingProcess - child process exited with code ${code}`);
-    });
-};
-
-
-class CamController {
-
-    constructor() {
-
-        console.log("new Cam instance...");
-
-    }
-
-    startRecording() {
-        if (this.camera) {
-            this.setStopListener(() => {
-                this.createNewCam();
-            })
-            this.camera.stop();
-        } else {
-            this.createNewCam();
-        }
-    }
-
-    createNewCam() {
-
-        this.camera = new RaspiCam({
-            mode: "video",
-            output: "/Records/",
-            timeout: 3600000
-            //framerate: 50
-        });
-
-        let d = new Date();
-        let time_string = d.toTimeString();
-        let time = time_string.split(' ')[0].replace(/:/g, "-");
-
-        let date = d.toString();
-        let dates = date.split(' ');
-        let date_string = dates[2].concat(dates[1].concat(dates[3]));
-
-        let full_date = date_string.concat("_".concat(time));
-
-        // Output-Directory: Anlegen, falls nicht schon vorhanden
-        let new_dir = "/../../../Records/";
-        let path = process.cwd().concat(new_dir);
-
-        console.log(path);
-        if (!fs.existsSync(path)) {
-            console.log("to create");
-            fs.mkdirSync(path);
-        }
-
-        let output = path.concat(full_date, ".h264");
-
-        this.camera.set("output", output);
-
-        killFFServerProcess(() => {
-            this.camera.start();
-
-            // callbacks
-            this.setAllListeners();
-        });
-    }
-
-    stopRecording() {
-        console.log("camera: stop recording!");
-        console.log("my output is: " + camera.get("output"));
-
-        if (this.camera) {
-            this.camera.stop();
-        }
-
-    }
-
-    setAllListeners() {
-        this.setStartListener();
-        this.setReadListener();
-        this.setExitListener();
-        this.setStopListener();
-    }
-
-    setStartListener() {
-        this.camera.on("start", (err, timestamp) => {
-            console.log("video started at " + timestamp);
-        });
-    }
-
-    setReadListener() {
-        this.camera.on("read", (err, timestamp, filename) => {
-            console.log("video captured with filename: " + filename + " at " + timestamp);
-        });
-    }
-
-    setExitListener() {
-        this.camera.on("exit", (timestamp) => {
-            console.log("video child process has exited at " + timestamp);
-            this.convertLastVideoToMp4();
-        });
-    }
-
-    setStopListener(callback) {
-        camera.on("stop", (err, timestamp) => {
-            console.log("video child process was stopped at " + timestamp);
-            startFFserverProcess();
-
-            callback();
-        });
-    }
-
-
-    /* convert .h264 into .mp4 */
-    convertLastVideoToMp4 = () => {
-        let outputH264 = camera.get("output");
-        let outputMp4 = outputH264.slice(0, outputH264.indexOf("h264")) + "mp4";
-
-        let ls = spawn('ffmpeg', ['-framerate', '50', '-i', outputH264, '-c', 'copy', outputMp4]);
-
-
-        ls.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-        });
-
-        ls.stderr.on('data', (data) => {
-            console.log(`stderr: ${data}`);
-        });
-
-        ls.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
-
-
-        //exec("ffmpeg -framerate 24 -i " + outputH264 + " -c copy " + outputMp4);
-    };
-
-
-}
